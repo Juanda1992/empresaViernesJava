1753121c6bac11dba2eaf14a4577907ad7a7e40b
503 <503@academicos.cesde.edu.co>
2021-09-17 08:35:00 -0500

    Se crea clase Empleado

---
+++ src/empleado/Empleado.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empleado;
+
+/**
+ *
+ * @author 503
+ */
+public class Empleado {
+    //ATRIBUTOS // DATOS // VARIABLES
+    int id;
+    String nombre;
+    String cargo;
+    int salario;
+    String telefono;
+    String direccion;
+    
+    //METODO ESPECIAL = FOTOCOPIADORA = INICIA CLASE
+     public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String cargo, int salario, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+     
+    // METODOS // ACCIONES // FUNCIONES
+
+    public int calcularSalario(){
+    
+        
+    return (0);
+    }
+
+}
