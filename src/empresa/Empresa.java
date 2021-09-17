52a37f2449e2b1e5f3a6b5881eb20e2fa7055124
503 <503@academicos.cesde.edu.co>
2021-09-17 10:00:00 -0500

    Se crea switch

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,6 +5,10 @@
  */
 package empresa;
 
+import empleado.Empleado;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  *
  * @author 503
@@ -15,7 +19,65 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        int contador =0;
+        
+        
+        Empleado [] empleados = new Empleado[10];
+        
+        do{
+            System.out.println("Bienvenidos");
+            System.out.println("***********");
+            System.out.println("1.Agregar Empleado");
+            System.out.println("2.Mostrar Empleado");
+            System.out.println("3.Mostar Empleados");
+            System.out.println("4. salir");
+            System.out.println("");
+            
+            System.out.println("Digite una opcion");
+            opcion=entrada.nextInt();
+            
+            switch (opcion) {
+                case 1:  
+                    //contador=0
+                    empleados[contador]= new Empleado();
+                    
+                    System.out.println("Digita el id: ");
+                    empleados[contador].setId(entrada.nextInt());
+                    System.out.println("Digita el nombre");
+                    empleados[contador].setNombre(entrada.next());
+                    System.out.println("Digite el cargo");
+                    empleados[contador].setCargo(entrada.next());
+                    System.out.println("Digite el salario");
+                    empleados[contador].setSalario(entrada.nextInt());
+                    System.out.println("Digite el telefono");
+                    empleados[contador].setTelefono (entrada.next());
+                    System.out.println("Digite la direccion");
+                    empleados[contador].setDireccion (entrada.next());
+                    
+                    contador ++;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    for (int i = 0; i < contador; i++) {
+                        System.out.println(empleados[i].getNombre());   
+                    }
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+           
+        }while(opcion!=4);
+        
+        
+        
     }
     
 }
